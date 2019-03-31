a285b3044b3328c2c831599dacafaafa01dc3681
msalvaris <msalvaris@users.noreply.github.com>
2019-03-31 11:10:09 +0100

    Refractoring utilities Removes unused methods associated with encoding

--- Keras_Tensorflow/testing_utilities.py
+++ Keras_Tensorflow/testing_utilities.py
@@ -1,102 +1,83 @@
-import base64
 import json
+import logging
+import random
+import time
 import urllib
 from io import BytesIO
-import time
 
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import toolz
 from PIL import Image, ImageOps
-import random
+from azureml.core.authentication import AuthenticationException, AzureCliAuthentication, InteractiveLoginAuthentication
 
-from azureml.core.authentication import ServicePrincipalAuthentication
-from azureml.core.authentication import AzureCliAuthentication
-from azureml.core.authentication import InteractiveLoginAuthentication
-from azureml.core.authentication import AuthenticationException
-from dotenv import set_key, get_key
-import logging
 
 def read_image_from(url):
-    return toolz.pipe(url, 
-                      urllib.request.urlopen,
-                      lambda x: x.read(),
-                      BytesIO)
+    return toolz.pipe(url, urllib.request.urlopen, lambda x: x.read(), BytesIO)
 
 
 def to_rgb(img_bytes):
-    return Image.open(img_bytes).convert('RGB')
+    return Image.open(img_bytes).convert("RGB")
 
 
 @toolz.curry
 def resize(img_file, new_size=(100, 100)):
     return ImageOps.fit(img_file, new_size, Image.ANTIALIAS)
 
-def to_bytes(img, encoding='JPEG'):
+
+def to_bytes(img, encoding="JPEG"):
     imgio = BytesIO()
     img.save(imgio, encoding)
     imgio.seek(0)
     return imgio.read()
-    
-def to_base64(img_bytes):
-    dataimg = base64.b64encode(img_bytes)
-    return dataimg.decode('utf-8')
 
 
 def to_img(img_url):
-    return toolz.pipe(img_url,
-                      read_image_from,
-                      to_rgb,
-                      resize(new_size=(224,224)))
+    return toolz.pipe(img_url, read_image_from, to_rgb, resize(new_size=(224, 224)))
 
 
-def img_url_to_json(url, label='image'):
-    img_data = toolz.pipe(url,
-                          to_img,
-                          to_base64)
-    return json.dumps({'input':{label:'\"{0}\"'.format(img_data)}})
-
-
-def  _plot_image(ax, img):
+def _plot_image(ax, img):
     ax.imshow(to_img(img))
-    ax.tick_params(axis='both',       
-                   which='both',      
-                   bottom=False,      
-                   top=False,         
-                   left=False,
-                   right=False,
-                   labelleft=False,
-                   labelbottom=False) 
+    ax.tick_params(
+        axis="both",
+        which="both",
+        bottom=False,
+        top=False,
+        left=False,
+        right=False,
+        labelleft=False,
+        labelbottom=False,
+    )
     return ax
 
 
 def _plot_prediction_bar(ax, r):
-    # perf = list(c[1] for c in r.json()[0]['image'])
-    perf = list(c[2] for c in r.json()[0]['image'])
-    perf = [float(i) for i in perf]
-    ax.barh(range(3, 0, -1), perf, align='center', color='#55DD55')
-    ax.tick_params(axis='both',       
-                   which='both',      
-                   bottom=False,      
-                   top=False,         
-                   left=False,
-                   right=False,
-                   labelbottom=False) 
-    # tick_labels = reversed(list(' '.join(c[0].split()[1:]).split(',')[0] for c in r.json()[0]['image']))
-    tick_labels = reversed(list(c[1] for c in r.json()[0]['image']))
-    ax.yaxis.set_ticks([1,2,3])
-    ax.yaxis.set_ticklabels(tick_labels, position=(0.5,0), minor=False, horizontalalignment='center')
+    perf = [float(c[2]) for c in r.json()[0]["image"]]
+    ax.barh(range(3, 0, -1), perf, align="center", color="#55DD55")
+    ax.tick_params(
+        axis="both",
+        which="both",
+        bottom=False,
+        top=False,
+        left=False,
+        right=False,
+        labelbottom=False,
+    )
+    tick_labels = reversed([c[1] for c in r.json()[0]["image"]])
+    ax.yaxis.set_ticks([1, 2, 3])
+    ax.yaxis.set_ticklabels(
+        tick_labels, position=(0.5, 0), minor=False, horizontalalignment="center"
+    )
 
 
-    
 def plot_predictions(images, classification_results):
-    if len(images)!=6:
-        raise Exception('This method is only designed for 6 images')
+    if len(images) != 6:
+        raise Exception("This method is only designed for 6 images")
     gs = gridspec.GridSpec(2, 3)
     fig = plt.figure(figsize=(12, 9))
     gs.update(hspace=0.1, wspace=0.001)
-    
-    for gg,r, img in zip(gs, classification_results, images):
+
+    for gg, r, img in zip(gs, classification_results, images):
         gg2 = gridspec.GridSpecFromSubplotSpec(4, 10, subplot_spec=gg)
         ax = fig.add_subplot(gg2[0:3, :])
         _plot_image(ax, img)
@@ -104,35 +85,28 @@ def plot_predictions(images, classification_results):
         _plot_prediction_bar(ax, r)
 
 
-        
-def write_json_to_file(json_dict, filename, mode='w'):
+def write_json_to_file(json_dict, filename, mode="w"):
     with open(filename, mode) as outfile:
-        json.dump(json_dict, outfile, indent=4,sort_keys=True)
-        outfile.write('\n\n')
-        
-def gen_variations_of_one_image(IMAGEURL, num, label='image'):
+        json.dump(json_dict, outfile, indent=4, sort_keys=True)
+        outfile.write("\n\n")
+
+
+def gen_variations_of_one_image(IMAGEURL, num):
     out_images = []
-    img = to_img(IMAGEURL).convert('RGB')
+    img = to_img(IMAGEURL).convert("RGB")
     # Flip the colours for one-pixel
     # "Different Image"
     for i in range(num):
         diff_img = img.copy()
-        rndm_pixel_x_y = (random.randint(0, diff_img.size[0]-1), 
-                          random.randint(0, diff_img.size[1]-1))
+        rndm_pixel_x_y = (
+            random.randint(0, diff_img.size[0] - 1),
+            random.randint(0, diff_img.size[1] - 1),
+        )
         current_color = diff_img.getpixel(rndm_pixel_x_y)
         diff_img.putpixel(rndm_pixel_x_y, current_color[::-1])
         out_images.append(to_bytes(diff_img))
     return out_images
 
-def wait_until_container_ready(somepredicate, timeout, period=0.25, *args, **kwargs):
-    mustend = time.time() + timeout
-    while time.time() < mustend:
-        if somepredicate(*args, **kwargs): return True
-        time.sleep(period)
-    return False
-
-def wait_until():
-    pass
 
 def get_auth():
     logger = logging.getLogger(__name__)
@@ -145,25 +119,28 @@ def get_auth():
         auth = InteractiveLoginAuthentication()
     return auth
 
-def wait_until_ready(endpoint,max_attempts):
-    
-    code=0
-    attempts=0
-    
-    while(code != 200):
+
+def wait_until_ready(endpoint, max_attempts):
+    code = 0
+    attempts = 0
+    while code != 200:
         attempts += 1
-        if(attempts == max_attempts):
+        if attempts == max_attempts:
             print("Unable to connect to endpoint, quitting")
-            raise Exception("Endpoint unavailable in " + str(max_attempts) + " attempts.")
+            raise Exception(
+                "Endpoint unavailable in " + str(max_attempts) + " attempts."
+            )
             break
         try:
             code = urllib.request.urlopen(endpoint).getcode()
         except Exception as error:
-            print("Exception caught opening endpoint :" + str(endpoint) + " " + str(error))
+            print(
+                "Exception caught opening endpoint :" + str(endpoint) + " " + str(error)
+            )
 
-        if(code != 200):
+        if code != 200:
             print("Endpoint unavailable, waiting")
             time.sleep(10)
 
-    output_str = "We are all done with code " + str(code) 
+    output_str = "We are all done with code " + str(code)
     return output_str
