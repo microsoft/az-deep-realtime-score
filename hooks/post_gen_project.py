245165965b67d3b3816a406e5de6bd5899c1c884
Ubuntu <mylogin@yanzdldevice.30im0rmuznlefezvtl1hotybhe.bx.internal.cloudapp.net>
2019-04-02 19:19:03 +0000

    dir structure

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,8 +11,8 @@ def remove_file(filepath):
 def remove_dir(dirpath):
     shutil.rmtree(os.path.join(PROJECT_DIRECTORY, dirpath))
 
-def move_files(subdir):
-    root = PROJECT_DIRECTORY
+def move_files(parentdir, subdir):
+    root = os.path.join(PROJECT_DIRECTORY,parentdir)
     for filename in os.listdir(os.path.join(root, subdir)):
         shutil.move(os.path.join(root, subdir, filename), os.path.join(root, filename))
         os.rmdir(os.path.join(root, subdir))
@@ -23,12 +23,12 @@ if __name__ == '__main__':
         remove_dir('./Pytorch')
         
         if '{{ cookiecutter.deployment_type }}' == 'aks':
-            remove_dir('./iotedge')
-            move_files('./aks')
+            remove_dir('./Keras_Tensorflow/iotedge')
+            move_files('./Keras_Tensorflow','./Keras_Tensorflow/aks')
 
         if '{{ cookiecutter.deployment_type }}' == 'iotedge':
-            remove_dir('./aks')
-            move_files('./iotedge')
+            remove_dir('./Keras_Tensorflow/aks')
+            move_files('./Keras_Tensorflow','./Keras_Tensorflow/iotedge')
 
     else:
         remove_dir('./Keras_Tensorflow')
